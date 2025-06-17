45fdb5025746fb4b8a8860ae4cd19a5c3507e707
Sam909001 <ademkaya1990kaya@gmail.com>
2025-06-17 10:20:27 -0700

    Create faq.js

---
+++ faq.js
@@ -0,0 +1,8 @@
+// FAQ Toggle Functionality
+document.querySelectorAll('.faq-question').forEach(button => {
+  button.addEventListener('click', () => {
+    const answer = button.nextElementSibling;
+    button.classList.toggle('active');
+    answer.classList.toggle('show');
+  });
+});
